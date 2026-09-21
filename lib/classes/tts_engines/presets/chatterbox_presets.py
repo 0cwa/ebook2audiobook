4dfbd69b1070da961dff53be1ca759824c9c3380
0cwa <kainau@yahoo.com>
2026-09-21 22:27:18 +0000

    feat(chatterbox): expose v2 and v3 presets

--- lib/classes/tts_engines/presets/chatterbox_presets.py
+++ lib/classes/tts_engines/presets/chatterbox_presets.py
@@ -1,16 +1,19 @@
-"""Presets for the isolated Chatterbox multilingual V2 worker."""
+"""Presets for the isolated Chatterbox multilingual V2/V3 worker."""
 
 from lib.conf_models import TTS_ENGINES, default_engine_settings
 
 
 _settings = default_engine_settings[TTS_ENGINES["CHATTERBOX"]]
 
+_base = {
+    "repo": _settings["repo"],
+    "samplerate": _settings["samplerate"],
+    "files": [],
+    "voice": _settings["voice"],
+    "voices": {},
+}
+
 models = {
-    "internal": {
-        "repo": _settings["repo"],
-        "samplerate": _settings["samplerate"],
-        "files": [],
-        "voice": _settings["voice"],
-        "voices": {},
-    }
+    "v2": {**_base, "variant": "v2"},
+    "v3": {**_base, "variant": "v3"},
 }
