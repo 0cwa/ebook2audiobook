7696cdc80f0c2187aa1afafd2dfed8ce9deb9126
0cwa <kainau@yahoo.com>
2026-09-24 21:15:18 +0000

    Add Turbo and Nano Chatterbox presets

--- lib/classes/tts_engines/presets/chatterbox_presets.py
+++ lib/classes/tts_engines/presets/chatterbox_presets.py
@@ -1,4 +1,4 @@
-"""Presets for the isolated Chatterbox multilingual V2/V3 worker."""
+"""Presets for the isolated Chatterbox worker profiles."""
 
 from lib.conf_models import TTS_ENGINES, default_engine_settings
 
@@ -6,8 +6,6 @@ from lib.conf_models import TTS_ENGINES, default_engine_settings
 _settings = default_engine_settings[TTS_ENGINES["CHATTERBOX"]]
 
 _base = {
-    "repo": _settings["repo"],
-    "lang": "multi",
     "samplerate": _settings["samplerate"],
     "files": [],
     "voice": _settings["voice"],
@@ -15,6 +13,46 @@ _base = {
 }
 
 models = {
-    "v2": {**_base, "variant": "v2"},
-    "v3": {**_base, "variant": "v3"},
+    "v2": {
+        **_base,
+        "repo": "ResembleAI/chatterbox",
+        "lang": "multi",
+        "variant": "v2",
+        "profile": "v2",
+        "loader_kind": "multilingual",
+        "family": "chatterbox-multilingual",
+        "supported_language_ids": tuple(_settings["languages"].values()),
+        "minimum_prompt_seconds": None,
+    },
+    "v3": {
+        **_base,
+        "repo": "ResembleAI/chatterbox",
+        "lang": "multi",
+        "variant": "v3",
+        "profile": "v3",
+        "loader_kind": "multilingual",
+        "family": "chatterbox-multilingual",
+        "supported_language_ids": tuple(_settings["languages"].values()),
+        "minimum_prompt_seconds": None,
+    },
+    "turbo": {
+        **_base,
+        "repo": "ResembleAI/chatterbox-turbo",
+        "lang": "eng",
+        "profile": "turbo",
+        "loader_kind": "turbo",
+        "family": "chatterbox-turbo",
+        "supported_language_ids": ("en",),
+        "minimum_prompt_seconds": 5.0,
+    },
+    "nano": {
+        **_base,
+        "repo": "ResembleAI/chatterbox-nano",
+        "lang": "eng",
+        "profile": "nano",
+        "loader_kind": "turbo",
+        "family": "chatterbox-turbo",
+        "supported_language_ids": ("en",),
+        "minimum_prompt_seconds": 5.0,
+    },
 }
